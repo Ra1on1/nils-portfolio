e8fa30965311a0b6bb837dd4c7b1ffe4ee2132b9
Nils <biewer.nils@gmail.com>
2025-02-24 15:52:49 +0100

    Scroll Down Animation und Texte angepasst

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -35,7 +35,7 @@ export const Contact = () => {
         <div className="px-4 w-150">
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
             {" "}
-            Get In Touch
+            Kontaktieren Sie mich
           </h2>
           <form className="space-y-6" onSubmit={handleSubmit}>
             <div className="relative">
@@ -63,7 +63,7 @@ export const Contact = () => {
                 value={formData.email}
                 className="w-full bg-white/5 border border-white/10 rounded 
               px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
-                placeholder="example@gmail.com"
+                placeholder="beispiel@gmail.com"
                 onChange={(e) =>
                   setFormData({ ...formData, email: e.target.value })
                 }
@@ -79,7 +79,7 @@ export const Contact = () => {
                 value={formData.message}
                 className="w-full bg-white/5 border border-white/10 rounded 
               px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
-                placeholder="Your Message..."
+                placeholder="Ihre Nachricht..."
                 onChange={(e) =>
                   setFormData({ ...formData, message: e.target.value })
                 }
@@ -90,7 +90,7 @@ export const Contact = () => {
               type="submit"
               className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
             >
-              Send Message
+              Nachricht senden
             </button>
           </form>
         </div>
